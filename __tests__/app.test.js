d188cd5eca14181a53ee8c69c7fd757f95b173c1
Tess Lameyer <tjlameyer@gmail.com>
2019-12-09 15:33:00 -0800

    GET with path / written and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-
+describe('app.js request handling', () => {
+  it('handles a GET request for path "/" by setting the body to the string "hi" and sending it to client-side', () => {
+    return request(app) 
+      .get('/')
+      .then(res => {
+        // console.log(res, 'this is the res from our server back to the client');
+        expect(res.text).toEqual('hi');
+      });
+  });
 });
